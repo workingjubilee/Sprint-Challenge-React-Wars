b1461f6cdf724ce7388e2e83170e2ab9e4cb640f
workingjubilee <workingjubilee@gmail.com>
2019-02-15 10:00:42 -0800

    Much prettier now.

--- starwars/src/components/RebelScum.js
+++ starwars/src/components/RebelScum.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const RebelScum = scum => {
   return (
-    <li><h2><span className="WANTED">WANTED</span>  <br />
+    <li>
+    <ul className="innerList">
+    <h2><span className="WANTED">WANTED</span>  <br />
     {scum.name}</h2>
     <h3>For High Treason</h3>
-    <ul className="innerList">
     <p>Height: <strong>{scum.height} cm</strong></p>
     <p>Mass: <strong>{scum.mass} kg</strong></p>
     <p>Hair: <strong>{scum.hair}</strong></p>
@@ -16,6 +17,6 @@ const RebelScum = scum => {
   );
 }
 
-// <p>Species: {scum.species}</p> <-- needs a GET and JSON conversion, probs
+// <p>Species: {scum.species}</p> <-- needs a GET and JSON conversion, probs?
 
 export default RebelScum;
